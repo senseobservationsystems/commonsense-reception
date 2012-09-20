bfc0d2f83ec068914c601a0ed6806927279f5815
Freek van Polen <freek@sense-os.nl>
2012-09-20 16:33:40 +0200

    Added LICENSE and copyright statements

--- reception.py
+++ reception.py
@@ -1,8 +1,18 @@
-'''
-Created on Aug 15, 2012
+""" 
+Copyright (©) [2012] Sense Observation Systems B.V.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
-@author: freek
-'''
 
 from wsgiref.simple_server import make_server
 import threading
@@ -30,7 +40,11 @@ class Reception():
         
         url     = environ.get('PATH_INFO', '/')
         method  = environ.get('REQUEST_METHOD', 'GET')
-        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        try:
+            request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        except:
+            request_body_size = 0
+        
         if request_body_size > 0:
             request_body = environ['wsgi.input'].read(request_body_size)
         else:
